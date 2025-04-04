389cf0ff46c583ed07ca7c5dbfdbc73158868d36
Nartan Kaplan <162456788+nartankaplan@users.noreply.github.com>
2025-04-05 02:23:32 +0300

    Update distribution_of_target_variable.py

--- Exploratory Data Analysis (EDA)/Class Imbalance Check/distribution_of_target_variable.py
+++ Exploratory Data Analysis (EDA)/Class Imbalance Check/distribution_of_target_variable.py
@@ -2,17 +2,14 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# CSV'yi oku
 df = pd.read_csv("secondary_data.csv", sep=';')
 
-# Target değişkenin dağılımını yazdır
 class_counts = df['class'].value_counts()
 print("🎯 Target Variable Distribution:\n")
 print(class_counts)
 print("\n🔢 Oranlar (%):")
 print(class_counts / class_counts.sum() * 100)
 
-# Görselleştirme
 plt.figure(figsize=(6, 4))
 sns.countplot(x='class', data=df, palette='Set2')
 plt.title("Class Distribution (e: Edible, p: Poisonous)")
